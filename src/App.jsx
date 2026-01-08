a0e62836c86cbb78c7d416776e01734d6889d7a2
Kudos Tech <abolajiayobami2000@gmail.com>
2026-01-08 15:52:48 +0000

    Finish About section and hero background layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import Aboutsection from './components/Aboutsection'
 import Navbar from './components/Navbar'
+import bg from './assets/bg.png'
 
 function App() {
-  
   return (
     <>
-   <Navbar/>
+      <div
+        className="w-full h-screen bg-cover bg-center bg-no-repeat"
+        style={{ backgroundImage: `url(${bg})` }}
+      >
+        <Navbar />
+              <Aboutsection />
+      </div>
     </>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,18 +11,18 @@ function Navbar() {
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
-    <nav className='mt-10'>
+    <nav className=''>
       {/* when on mobile */}
-      <div className='  mx-15 flex justify-between md:hidden'>
+      <div className=' mx-15 flex justify-between md:hidden'>
 
         <div className='flex items-center' >
-          <Link to="/" className=' gap-1 flex '>
+          <Link to="/" className=' gap-1 flex mt-10'>
             <img className='w-6' src={logo} alt="" />
             <p className='text-white font-bold' >NEXUM</p>
           </Link>
         </div>
 
-        <div className="text-white cursor-pointer"
+        <div className="text-white cursor-pointer mt-10"
           onClick={() => setMenuOpen(!menuOpen)}
         >
           {menuOpen ? (
@@ -36,22 +36,22 @@ function Navbar() {
       {menuOpen && (
   <ul className="md:hidden text-xl mt-15 items-center flex flex-col gap-10 text-white font-semibold px-5">
     <li><a href="">About</a></li>
-    <li><a href="">NEXM Token</a></li>
-    <li><a href="">Vision</a></li>
-    <li><a href="">How to buy</a></li>
-    <li><a href="">Blog</a></li>
+    <li><a href="">Features</a></li>
     <li><a href="">Team</a></li>
     <li><a href="">FAQ</a></li>
+    <li><a href="">Blog</a></li>
+    <li><a href="">Tokens</a></li>
+
   </ul>
 )}
 
       {/* when on tablet */}
       <div className=' hidden md:flex lg:hidden items-center  ' >
-        <Link to="/" className='gap-1 flex ml-5 '>
+        <Link to="/" className='gap-1  mt-10 flex ml-5 '>
           <img className='w-6' src={logo} alt="" />
           <p className='text-white font-bold'> NEXUM </p>
         </Link>
-        <ul className='ml-12 hidden md:flex gap-4
+        <ul className='ml-12 hidden md:flex  mt-10 gap-4
 font-light text-white items-center '>
           <li>
             <a href=""> About</a>
@@ -77,19 +77,19 @@ font-light text-white items-center '>
         </ul>
 
         <div>
-          <button className=' hidden md:flex ml-10 text-white border px-4 py-2 rounded-md border-[#04adc0] border-l-[#01C09D]'> Contact</button>       </div>
+          <button className=' hidden md:flex ml-12 text-white border px-4 py-2 rounded-md  mt-10 border-[#04adc0] border-l-[#01C09D] cursor-pointer'> Contact</button>       </div>
       </div>
 
 
       {/* when on desktop */}
-      <div className=' mx-20 hidden md:hidden justify-between lg:flex items-center  ' >
-        <Link to="/" className='gap-1 flex '>
+      <div className=' mx-20  hidden md:hidden justify-between lg:flex items-center  ' >
+        <Link to="/" className='gap-1 flex mt-10 '>
           <img className='w-6' src={logo} alt="" />
           <p className='text-white font-bold'> NEXUM </p>
         </Link>
 
         <ul className='hidden md:flex gap-12
-font-light text-white items-center ml-5 '>
+font-light text-white items-center ml-5 mt-10'>
           <li>
             <a href=""> About</a>
           </li>
@@ -114,13 +114,12 @@ font-light text-white items-center ml-5 '>
         </ul>
 
         <Link to="/">
-          <button className='cursor-pointer hidden md:flex ml-10 text-white border px-4 py-2 rounded-md border-[#04adc0] border-l-[#01C09D]'> Contact</button>
+          <button className='hidden md:flex ml-10 text-white border px-4 py-2 rounded-md border-[#04adc0] border-l-[#01C09D] mt-10 cursor-pointer '> Contact</button>
         </Link>
       </div>
     </nav>
   )
 };
-
 export default Navbar;
 
 
